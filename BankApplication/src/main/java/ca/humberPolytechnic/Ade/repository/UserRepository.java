fe6415116127b145d3ca48c9cfc4f428f49b606b
AdedolapoVegaz <dolapohazan@yahoo.com>
2026-04-23 19:22:13 -0400

    Delete BankApplication directory

--- BankApplication/src/main/java/ca/humberPolytechnic/Ade/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ca.humberPolytechnic.Ade.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ca.humberPolytechnic.Ade.model.AppUser;
-
-public interface UserRepository extends JpaRepository<AppUser, Long>{
-	AppUser findByUsername(String username);
-}
